d5686ccd43f72e6243d5851e1cbbb52fcaaac60d
HojunXie <hojun.xie2@gmail.com>
2023-11-03 00:30:28 +0800

    Update hw5-2

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main () {
+  int minutes;
+  scanf("%d", &minutes);
+  double fee = minutes * 0.9;
+  if (minutes >= 1500) {
+    fee *= 79/100;
+  } else if (minutes >= 800) {
+    fee *= 0.9;
+  }
+  printf("%0.1f", fee);
+  return 0;
+}
